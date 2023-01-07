c604e58a4fb2b00f3ec589d5d9511159fd4e8653
Abdullah Yahya <abdullah747@gmail.com>
2023-01-07 10:16:36 -0800

    Added code to test env variables

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,5 +1,10 @@
+require('dotenv').config();
+
 module.exports = function() {
     return {
-        "variable11": "value11"
+        "environment": process.env.ENVIRONMENT,
+        "db_host": process.env.DB_HOST,
+        "db_user": process.env.DB_USER,
+        "db_password": process.env.DB_PASSWORD
     }
 };
