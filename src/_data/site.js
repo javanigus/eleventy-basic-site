87a749e8854c0e58aefa9322244338a9ee4a208e
Abdullah Yahya <ayahya@qualys.com>
2023-05-18 15:38:57 -0700

    Fixed path to local .env file

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,4 +1,6 @@
-require('dotenv').config();
+const path = require('path');
+const dotenv = require('dotenv');
+dotenv.config({ path: path.resolve(__dirname, '../../.env') })
 
 module.exports = function() {
     return {
